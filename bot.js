889079c0183f527620838237c45a901aba1e73f6
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-27 15:18:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
-const Canvas = require("canvas");
 const jimp = require("jimp");
 const moment = require('moment');
 const yt = require('ytdl-core');
